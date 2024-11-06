fe533e79f0b1b403c98048e31e2e89634a7d620d
Victor Fernandes <vfsomente@gmail.com>
2024-11-06 17:28:26 -0300

    Atualização 1.2
    Adicionado Main component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from './componets/header/header.component';
+import { HeaderComponent } from './components/header/header.component';
+import { MainComponent } from './components/main/main.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent ],
+  imports: [RouterOutlet, HeaderComponent, MainComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'RedeAcademias';
 }
+
