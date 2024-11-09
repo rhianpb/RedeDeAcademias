7c81a40200906fc5f33136efc46f4824bced6a2e
Victor Fernandes <vfsomente@gmail.com>
2024-11-09 14:16:21 -0300

    teste 1.0

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { CadastroComponent } from './pages/cadastro/cadastro.component';
+import { HeaderComponent } from './components/header/header.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'cadastro', component: CadastroComponent },
+    {path: '', component: HeaderComponent},
+];
