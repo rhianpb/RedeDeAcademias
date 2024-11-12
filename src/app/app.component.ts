d30199615e9f1f6c7c5407a7f1f974f1b9c8ef43
Victor Fernandes <vfsomente@gmail.com>
2024-11-12 19:41:32 -0300

    Atualização 1.4
    Adicionado Sessão "Serviços"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { MainComponent } from './components/main/main.component';
+import { ResourcesComponent } from "./components/resources/resources.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, MainComponent],
+  imports: [HeaderComponent, MainComponent, ResourcesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
