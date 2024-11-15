942667aa3b31f7ec7f72a31c7689285da8b1b813
Victor Fernandes <154020731+VFSomente@users.noreply.github.com>
2024-11-15 19:54:37 -0300

    Atualização 1.6

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { MainComponent } from './components/main/main.component';
 import { Section2Component } from './components/section2/section2.component';
@@ -8,7 +7,7 @@ import { Section2Component } from './components/section2/section2.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, MainComponent, Section2Component],
+  imports: [HeaderComponent, MainComponent, Section2Component],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
